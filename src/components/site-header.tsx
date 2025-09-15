cb90ad1de25a3dcfcccf876df611c7b7354452f3
Kelvin Feh <k3lvincodes@gmail.com>
2025-09-15 01:01:22 +0000

    Shift Home Features Pricing About Resources  to the center of navbar

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -36,10 +36,13 @@ export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background">
       <div className="container flex h-14 items-center">
-        <div className="mr-4 flex items-center">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
+        <div className="flex items-center">
+          <Link href="/" className="flex items-center space-x-2">
             <span className="font-bold font-headline text-lg">Collectii.</span>
           </Link>
+        </div>
+        
+        <div className="flex-1 flex justify-center">
           <nav className="hidden gap-6 text-sm md:flex">
             {navItems.map((item) => (
               <Link
@@ -67,7 +70,8 @@ export function SiteHeader() {
             </DropdownMenu>
           </nav>
         </div>
-        <div className="flex flex-1 items-center justify-end gap-2">
+
+        <div className="flex items-center justify-end gap-2">
           <Button variant="ghost" asChild>
             <Link href="/sign-in">Sign In</Link>
           </Button>
