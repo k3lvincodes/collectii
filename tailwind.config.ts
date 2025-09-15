c010b8dcb400d829a044a91698d4b8f0eb326450
Kelvin Feh <k3lvincodes@gmail.com>
2025-09-15 00:44:56 +0000

    use this as the font for everything in the website: <link rel="preconnec

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Josefin Sans', 'sans-serif'],
+        headline: ['Josefin Sans', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
