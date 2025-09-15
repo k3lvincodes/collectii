1ef06803ca8a9e0f7b4920af6b24956085c6fc93
Kelvin Feh <k3lvincodes@gmail.com>
2025-09-15 02:33:36 +0000

    the distance between the right border of the sidebar and the main conten

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,7 +4,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full">
       <AppSidebar />
-      <div className="flex flex-col md:ml-[calc(220px+10px)] lg:ml-[calc(280px+10px)]">
+      <div className="flex flex-col md:ml-[calc(220px-30px)] lg:ml-[calc(280px-30px)]">
         {children}
       </div>
     </div>
