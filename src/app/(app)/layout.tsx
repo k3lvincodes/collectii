27e6968b81431010934634d2224dbe6a7c53e471
Kelvin Feh <k3lvincodes@gmail.com>
2025-09-15 02:28:38 +0000

    make the main content page to fill the rest of the page, from the right

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,9 +2,9 @@ import { AppSidebar } from '@/components/app-sidebar';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="w-full">
       <AppSidebar />
-      <div className="flex flex-col">
+      <div className="flex flex-col md:ml-[220px] lg:ml-[280px]">
         {children}
       </div>
     </div>
